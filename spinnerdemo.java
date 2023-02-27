e05c20fdc02f65b1c2b15ca2a553f0433c377c53
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2023-02-27 10:54:43 +0000

    Create spinnerdemo.java

---
+++ spinnerdemo.java
@@ -0,0 +1,25 @@
+import androix.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter; 
+import android.widget.AdapterView; 
+import android.content.Intent; 
+import android.view.View; 
+
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener { 
+  Spinner mySpinner; 
+  String myArray[] = {"Android","IOs","React-Native}; 
+  
+  { 
+    mySpinner = findViewById(R.id.spinner_1); 
+    mySpinner.setOnItemSelectedListener(this);
+    ArrayAdapter myItems = new ArrayAdapter(this,android.R.layout.simple_spinner_item,myArray); 
+    myItems.setDropDownViewResource(android.R.layout.simple_dropdown_spinner_item); 
+    mySpinner.setAdapter(myItems);
+  }
+                     }
+    
+   
